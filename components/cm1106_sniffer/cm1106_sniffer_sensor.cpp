795ae18f75aada69c211e4c2dc5edb4ec2bb1634
joopmartens <joopmartens@hotmail.com>
2025-08-23 14:58:52 +0200

    Hardwareserial

--- components/cm1106_sniffer/cm1106_sniffer_sensor.cpp
+++ components/cm1106_sniffer/cm1106_sniffer_sensor.cpp
@@ -13,12 +13,13 @@ namespace cm1106_sniffer {
 
 static const char *TAG = "cm1106_sniffer";
 
+HardwareSerial cm1106Serial(1);
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // We use the secondary cm1106Serial (UART) to receive the data from the CM1106 sensor.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void CM1106SnifferSensor::setup() {
   //cm1106Serial.begin(9600);
-  HardwareSerial cm1106Serial(1);
   cm1106Serial.begin(9600, SERIAL_8N1, 21, 20);
 }
 
