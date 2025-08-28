e20b6cd83dc314d5e3085fa08de750c7580a0d08
joopmartens <joopmartens@hotmail.com>
2025-08-28 23:47:28 +0200

    v2

--- components/cm1106_sniffer/sensor.py
+++ components/cm1106_sniffer/sensor.py
@@ -1,58 +1,60 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, uart
+from esphome.components import uart, sensor
 from esphome.const import (
     CONF_ID,
-    CONF_UPDATE_INTERVAL,
+    CONF_CO2,
+    CONF_UART_ID,
     UNIT_PARTS_PER_MILLION,
     ICON_MOLECULE_CO2,
-    STATE_CLASS_MEASUREMENT,
     DEVICE_CLASS_CARBON_DIOXIDE,
+    STATE_CLASS_MEASUREMENT,
+    CONF_ACCURACY_DECIMALS,
+    CONF_ICON,
+    CONF_UNIT_OF_MEASUREMENT,
 )
 
-# Define the namespace for the C++ component
+# Define the namespace for the component
+# This should match the namespace in your C++ code
 cm1106_sniffer_ns = cg.esphome_ns.namespace("cm1106_sniffer")
 
-# Explicitly declare the C++ class from the header file
-CM1106Sniffer = cm1106_sniffer_ns.class_("CM1106Sniffer", sensor.Sensor, cg.PollingComponent)
+# Declare the C++ class name and its base classes
+CM1106Sniffer = cm1106_sniffer_ns.class_("CM1106SnifferSensor", cg.PollingComponent, uart.UARTDevice)
 
-# Define the configuration schema for the component
-CONFIG_SCHEMA = (
-    sensor.sensor_schema(
+# Dependencies for the component
+DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["sensor"]
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
         {
-            "unit_of_measurement": UNIT_PARTS_PER_MILLION,
-            "icon": ICON_MOLECULE_CO2,
-            "accuracy_decimals": 0,
-            "device_class": DEVICE_CLASS_CARBON_DIOXIDE,
-            "state_class": STATE_CLASS_MEASUREMENT,
-        }
-    )
-    .extend(
-        {
+            # Component ID
             cv.GenerateID(): cv.declare_id(CM1106Sniffer),
-            # The component requires the UART bus ID to connect to
-            cv.Required(uart.CONF_UART_ID): cv.use_id(uart.UARTComponent),
+            # The component requires a UART ID to connect to
+            cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+            # Define an optional CO2 sensor within the component
+            cv.Optional(CONF_CO2): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PARTS_PER_MILLION,
+                icon=ICON_MOLECULE_CO2,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_CARBON_DIOXIDE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
         }
     )
     .extend(cv.polling_component_schema("10s"))
 )
 
-
-def to_yaml(config):
-    # This function is not strictly needed for this simple sensor, but is
-    # good practice for more complex components to handle YAML output.
-    pass
-
-
-async def to_code(config):
-    # Get the UART bus ID from the configuration
+def to_code(config):
+    # Create an instance of the C++ class
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    yield cg.register_component(var, config)
 
-    # Get a reference to the UART component variable
-    uart_component = await cg.get_variable(config[uart.CONF_UART_ID])
-    # Call the setter method on the C++ component
-    cg.add(var.set_uart_parent(uart_component))
+    # Get the UART component instance and set it in our C++ component
+    parent = yield cg.get_variable(config[CONF_UART_ID])
+    cg.add(var.set_uart_device(parent))
 
-    # Use a sensor schema
-    await sensor.register_sensor(var, config)
+    # If the user defined a CO2 sensor in the YAML, create it and set it
+    if CONF_CO2 in config:
+        sens = yield sensor.new_sensor(config[CONF_CO2])
+        cg.add(var.set_co2_sensor(sens))
