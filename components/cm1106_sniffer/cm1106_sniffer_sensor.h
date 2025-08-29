f13d0de34c18ae5079032eb479146add324442dc
joopmartens <joopmartens@hotmail.com>
2025-08-29 19:17:34 +0200

    v2

--- components/cm1106_sniffer/cm1106_sniffer_sensor.h
+++ components/cm1106_sniffer/cm1106_sniffer_sensor.h
@@ -3,31 +3,27 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/component.h"
-#include "esphome/core/helpers.h"
 
 namespace esphome {
 namespace cm1106_sniffer {
 
-class CM1106Sniffer : public sensor::Sensor, public PollingComponent {
+class CM1106Sniffer : public PollingComponent, public uart::UARTDevice {
  public:
+  void set_co2_sensor(sensor::Sensor *co2_sensor) { this->co2_sensor_ = co2_sensor; }
+  void set_uart_bus(uart::UARTComponent *uart_bus) { this->set_uart_parent(uart_bus); }
+
   void setup() override;
   void loop() override;
   void dump_config() override;
-  void update() override;
-
-  void set_uart_parent(uart::UARTComponent *uart_parent) { this->uart_component_ = uart_parent; }
 
  protected:
   void handle_byte(uint8_t byte);
   void reset_buffer_();
-  
-  uart::UARTComponent *uart_component_{nullptr};
+
+  sensor::Sensor *co2_sensor_{nullptr};
   uint8_t buffer_[9];
   uint8_t buffer_pos_{0};
-  uint16_t co2_value_ = 0;
-  bool should_update_ = true;
-  bool frame_ready_ = false;
 };
 
-}  // namespace cm1106_sniffer
-}  // namespace esphome
+} // namespace cm1106_sniffer
+} // namespace esphome
