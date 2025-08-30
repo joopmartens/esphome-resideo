a08f70d5407a6dc58f584bb26bfb636e271fba3a
joopmartens <joopmartens@hotmail.com>
2025-08-30 12:11:49 +0200

    v2

--- components/cm1106_sniffer/sensor.py
+++ components/cm1106_sniffer/sensor.py
@@ -4,36 +4,40 @@ from esphome.components import sensor, uart
 from esphome.const import (
     CONF_ID,
     CONF_UART_ID,
+    CONF_CO2,
     CONF_UPDATE_INTERVAL,
     UNIT_PARTS_PER_MILLION,
     ICON_MOLECULE_CO2,
     DEVICE_CLASS_CARBON_DIOXIDE,
     STATE_CLASS_MEASUREMENT,
     CONF_ACCURACY_DECIMALS,
-    CONF_NAME,
-    CONF_UNIT_OF_MEASUREMENT,
-    CONF_ICON,
-    CONF_DEVICE_CLASS,
-    CONF_STATE_CLASS
 )
 
 # Define the namespace for the component
 cm1106_sniffer_ns = cg.esphome_ns.namespace("cm1106_sniffer")
 
 # Declare the C++ class name and its base classes
-CM1106Sniffer = cm1106_sniffer_ns.class_("CM1106Sniffer", sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
+CM1106SnifferComponent = cm1106_sniffer_ns.class_(
+    "CM1106SnifferComponent", cg.PollingComponent, uart.UARTDevice
+)
 
-# Dependencies for the component
-DEPENDENCIES = ["uart"]
+# The configuration schema for the nested CO2 sensor
+CO2_SENSOR_SCHEMA = (
+    sensor.sensor_schema(
+        UNIT_PARTS_PER_MILLION, ICON_MOLECULE_CO2, 0, DEVICE_CLASS_CARBON_DIOXIDE, STATE_CLASS_MEASUREMENT
+    ).extend({})
+)
 
-# Define the configuration schema to extend the sensor base schema
-CONFIG_SCHEMA = cv.All(
-    sensor.SENSOR_SCHEMA.extend(
+# Main configuration schema for the component
+CONFIG_SCHEMA = (
+    cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(CM1106Sniffer),
+            cv.GenerateID(): cv.declare_id(CM1106SnifferComponent),
             cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+            cv.Optional(CONF_CO2): CO2_SENSOR_SCHEMA,
         }
-    ).extend(cv.polling_component_schema(CONF_UPDATE_INTERVAL))
+    )
+    .extend(cv.polling_component_schema(CONF_UPDATE_INTERVAL))
 )
 
 def to_code(config):
@@ -41,16 +45,7 @@ def to_code(config):
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
 
-    # Manually set the sensor properties on the component
-    if CONF_UNIT_OF_MEASUREMENT in config:
-        cg.add(var.set_unit_of_measurement(config[CONF_UNIT_OF_MEASUREMENT]))
-    if CONF_ICON in config:
-        cg.add(var.set_icon(config[CONF_ICON]))
-    if CONF_DEVICE_CLASS in config:
-        cg.add(var.set_device_class(config[CONF_DEVICE_CLASS]))
-    if CONF_STATE_CLASS in config:
-        cg.add(var.set_state_class(config[CONF_STATE_CLASS]))
-    if CONF_ACCURACY_DECIMALS in config:
-        cg.add(var.set_accuracy_decimals(config[CONF_ACCURACY_DECIMALS]))
-    if CONF_NAME in config:
-        cg.add(var.set_name(config[CONF_NAME]))
+    if CONF_CO2 in config:
+        conf = config[CONF_CO2]
+        sens = yield sensor.new_sensor(conf)
+        cg.add(var.set_co2_sensor(sens))
