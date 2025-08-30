7e3684eac2027729cbcd8f8936d254a47f99df13
joopmartens <joopmartens@hotmail.com>
2025-08-30 09:40:56 +0200

    v2

--- components/cm1106_sniffer/sensor.py
+++ components/cm1106_sniffer/sensor.py
@@ -11,6 +11,7 @@ from esphome.const import (
     DEVICE_CLASS_CARBON_DIOXIDE,
     STATE_CLASS_MEASUREMENT,
     CONF_ACCURACY_DECIMALS,
+    CONF_NAME,
 )
 
 # Define the namespace for the component
@@ -35,7 +36,9 @@ CONFIG_SCHEMA = cv.All(
                 accuracy_decimals=0,
                 device_class=DEVICE_CLASS_CARBON_DIOXIDE,
                 state_class=STATE_CLASS_MEASUREMENT,
-            ),
+            ).extend({
+                cv.Optional(CONF_NAME): cv.string
+            }),
         }
     ).extend(cv.polling_component_schema(CONF_UPDATE_INTERVAL))
 )
@@ -44,9 +47,24 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
 
-    parent = yield cg.get_variable(config[uart.CONF_UART_ID])
+    parent = yield cg.get_variable(config[CONF_UART_ID])
     cg.add(var.set_uart_bus(parent))
 
     if CONF_CO2 in config:
-        sens = yield sensor.new_sensor(config[CONF_CO2])
+        conf_co2 = config[CONF_CO2]
+        sens = yield sensor.new_sensor(conf_co2)
         cg.add(var.set_co2_sensor(sens))
+        
+        # Explicitly set the sensor properties
+        if CONF_NAME in conf_co2:
+            cg.add(sens.set_name(conf_co2[CONF_NAME]))
+        if CONF_UNIT_OF_MEASUREMENT in conf_co2:
+            cg.add(sens.set_unit_of_measurement(conf_co2[CONF_UNIT_OF_MEASUREMENT]))
+        if CONF_ICON in conf_co2:
+            cg.add(sens.set_icon(conf_co2[CONF_ICON]))
+        if CONF_ACCURACY_DECIMALS in conf_co2:
+            cg.add(sens.set_accuracy_decimals(conf_co2[CONF_ACCURACY_DECIMALS]))
+        if CONF_DEVICE_CLASS in conf_co2:
+            cg.add(sens.set_device_class(conf_co2[CONF_DEVICE_CLASS]))
+        if CONF_STATE_CLASS in conf_co2:
+            cg.add(sens.set_state_class(conf_co2[CONF_STATE_CLASS]))
