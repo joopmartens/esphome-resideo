665e0df30f48e81badf7a5aac1189b65b23312a3
joopmartens <joopmartens@hotmail.com>
2025-08-30 12:57:33 +0200

    v2

--- components/cm1106_sniffer/cm1106_sniffer_sensor.h
+++ components/cm1106_sniffer/cm1106_sniffer_sensor.h
@@ -3,33 +3,32 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/core/log.h"
-#include "esphome/core/helpers.h"
-#include <vector>
-#include <algorithm>
-#include <string.h>
 
 namespace esphome {
 namespace cm1106_sniffer {
 
-class CM1106SnifferSensor : public PollingComponent, public uart::UARTDevice {
+class CM1106Sniffer : public esphome::PollingComponent, public uart::UARTDevice {
  public:
-  // Lifecycle methods for ESPHome.
+  // Public methods required by ESPHome
   void setup() override;
   void loop() override;
   void dump_config() override;
   void update() override;
 
-  // Setter to link the component to the ESPHome sensor.
+  // Setters to link the component to the ESPHome sensor and UART bus.
   void set_co2_sensor(sensor::Sensor *sensor) { this->co2_sensor_ = sensor; }
+  void set_uart_bus(uart::UARTComponent *bus) { this->uart_bus_ = bus; }
 
  protected:
+  // Private helper methods for handling the data stream
   void handle_byte(uint8_t byte);
   void reset_buffer_();
 
+  // Member variables for the state machine
   uint8_t buffer_[9] = {0};
-  uint8_t buffer_pos_ = 0;
+  uint8_t buffer_pos_{0};
   sensor::Sensor *co2_sensor_{nullptr};
+  uart::UARTComponent *uart_bus_{nullptr};
 };
 
 }  // namespace cm1106_sniffer
